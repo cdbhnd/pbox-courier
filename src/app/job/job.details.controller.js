b969cfb443ae369ccd68c1f0d6c8e36ac0b8eb27
Ognjen Tomic <gliamanti@gmail.com>
2016-12-28 15:44:26 +0100

    PB-48 job details page and cancel button added:

---
+++ src/app/job/job.details.controller.js
@@ -0,0 +1,64 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pbox.courier.job')
+        .controller('jobDetailsController', jobDetailsController);
+
+    /** @ngInject */
+    function jobDetailsController($scope, $q, $timeout, $ionicPopup, jobService, pboxLoader, pboxAlert, $stateParams) {
+
+        var vm = this;
+
+        // public methods
+        vm.cancelJob = cancelJob;
+
+        //variables and properties
+        vm.job = null;
+
+        /////////////////////////////////////
+
+        (function activate() {
+            startLoading()
+                .then(loadJob)
+                .finally(stopLoading);
+        }());
+
+        /////////////////////////////////////
+
+        function loadJob() {
+            return jobService.getJob($stateParams.jobId)
+                .then(function(response) {
+                    vm.job = response;
+                    if (response.length == 0) {
+                        pboxAlert.alert('Job could not be found !');
+                    }
+                });
+        }
+
+        function cancelJob() {
+            jobService.updateJob($stateParams.jobId, {
+                "status": "CANCELED"
+            })
+            .then(function(response){
+                console.log(response);
+                pboxAlert.alert('Job canceled!');
+            })
+            .catch(function(err){
+                pboxAlert.alert('Job could not be canceled!');
+            });
+        }
+
+        function startLoading() {
+            return $q.when(function() {
+                pboxLoader.loaderOn();
+            });
+        }
+
+        function stopLoading() {
+            return $q.when(function() {
+                pboxLoader.loaderOff();
+            });
+        }
+    }
+})();
--- src/app/job/job.service.js
+++ src/app/job/job.service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     angular
         .module('pbox.courier.job')
         .service('jobService', jobService);
@@ -8,18 +8,20 @@
         var service = this;
 
         service.getAll = getAllJobs;
+        service.getJob = getJob;
+        service.updateJob = updateJob;
 
         ///////////////////////////////////////////
 
         function getAllJobs() {
             return pboxApi.http({
-                    method: config.httpMethods.GET,
-                    url: config.pboxAPI.JOBS,
-                    params: {
-                        "status": config.jobStatus.PENDING
-                    }
-                })
-                .then(function(response) {
+                method: config.httpMethods.GET,
+                url: config.pboxAPI.JOBS,
+                params: {
+                    "status": config.jobStatus.PENDING
+                }
+            })
+                .then(function (response) {
                     var jobs = [];
 
                     if (response.length) {
@@ -30,6 +32,37 @@
 
                     return jobs;
                 });
+        }
+
+        function getJob(jobId) {
+            return pboxApi.http({
+                method: config.httpMethods.GET,
+                url: config.pboxAPI.JOBS,
+                params: {
+                    "id": jobId
+                }
+            })
+            .then(function (response) {
+                return response[0];
+            });
+        }
+
+        function updateJob(jobId, query) {
+            return pboxApi.http({
+                method: config.httpMethods.PUT,
+                url: config.pboxAPI.JOBS + '/' + jobId,
+                data: query
+            })
+            .then(function (response) {
+                var jobs = [];
+
+                if (response.length) {
+                    for (var i = 0; i < response.length; i++) {
+                        jobs[i] = new JobModel(response[i])
+                    }
+                }
+                return jobs;
+            });
         }
 
     }
