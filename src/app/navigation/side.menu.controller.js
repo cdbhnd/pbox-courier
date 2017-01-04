cb3c6bdf0fdee682533abfc9a39d6c0ce9d5673d
uros pajic <uros pajic>
2017-01-04 09:02:24 +0100

    PB-30 side menu implemented and designed

--- src/app/navigation/side.menu.controller.js
+++ src/app/navigation/side.menu.controller.js
@@ -6,13 +6,14 @@
 		.controller('sideMenuController', sideMenuController);
 
 	/** @ngInject */
-	function sideMenuController($q, $rootScope, $state) {
+	function sideMenuController($q, $rootScope, $state, authService) {
 
 		var vm = this;
 
 		vm.currentState = null;
 
 		vm.changeState = changeState;
+		vm.logout = logout;
 
 		(function activate() {
 			subscribeOnCurrentStateChnage();
@@ -26,6 +27,13 @@
 
 		function changeState(name, params) {
 			return $state.go(name, params);
+		}
+
+		function logout() {
+			return authService.logout()
+				.then(function() {
+					$state.go('login');
+				});
 		}
 	}
 })();
