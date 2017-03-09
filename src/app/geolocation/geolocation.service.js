dfb3609aaea9c0e15a37538f0a0b69067d940ac7
Ognjen Tomic <gliamanti@gmail.com>
2017-03-09 11:31:56 +0100

    random location generator added

--- src/app/geolocation/geolocation.service.js
+++ src/app/geolocation/geolocation.service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -6,7 +6,7 @@
         .service('geolocationService', geolocationService);
 
     /** @ngInject */
-    function geolocationService($q, $rootScope, GeolocationModel, $cordovaGeolocation) {
+    function geolocationService($q, $rootScope, GeolocationModel, $cordovaGeolocation, config) {
         var service = this;
 
         service.init = init;
@@ -17,52 +17,17 @@
         //////////////////////////////
 
         function init() {
-            var posOptions = {
-                timeout: 10000,
-                enableHighAccuracy: false
-            };
-            $cordovaGeolocation
-                .getCurrentPosition(posOptions)
-                .then(function(position) {
-                    _currentLocation = new GeolocationModel({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude
-                    });
-                    $rootScope.current_location = _currentLocation;
-                    console.log('Location fetch: ');
-                    console.log(_currentLocation);
-                }, function(err) {
-                    console.log(err.message);
-                    setFallbackCoordinates(err.message);
-                });
-
-            var watchOptions = {
-                timeout: 3000,
-                enableHighAccuracy: false // may cause errors if true
-            };
-            $cordovaGeolocation.watchPosition(watchOptions)
-                .then(null,
-                    function(err) {
-                        console.log(err.message);
-                        if (!_currentLocation) {
-                            setFallbackCoordinates(err.message);
-                        }
-                    },
-                    function(position) {
-                        _currentLocation = new GeolocationModel({
-                            latitude: position.coords.latitude,
-                            longitude: position.coords.longitude
-                        });
-                        $rootScope.current_location = _currentLocation;
-                        console.log('Location watch: ');
-                        console.log(_currentLocation);
-                    }
-                );
+            if (isDemoLocationEnabled()) {
+                _currentLocation = getDemoLocation();
+                $rootScope.current_location = _currentLocation;
+            } else {
+                getCurrentLocation();
+            }
         }
 
         function currentLocation() {
-            return $q.when(function() {
-                while (!_currentLocation) {}
+            return $q.when(function () {
+                while (!_currentLocation) { }
                 return _currentLocation;
             }());
         }
@@ -104,6 +69,67 @@
                 latitude: newlat.toFixed(8),
                 longitude: newlon.toFixed(8)
             };
+        }
+        function isDemoLocationEnabled() {
+            return !!config.demoLocation && (config.demoLocation.hardcoded || config.demoLocation.random);
+        }
+
+        function getDemoLocation() {
+            var geoLocationModel = new GeolocationModel();
+
+            if (config.demoLocation.hardcoded) {
+                geoLocationModel.latitude = config.demoLocation.lat;
+                geoLocationModel.longitude = config.demoLocation.lng;
+            } else if (config.demoLocation.random) {
+                var randomCordinates = randomGeo({ latitude: config.demoLocation.lat, longitude: config.demoLocation.lng }, config.demoLocation.radius);
+                geoLocationModel.latitude = randomCordinates.latitude;
+                geoLocationModel.longitude = randomCordinates.longitude;
+            }
+            return geoLocationModel;
+        }
+
+        function getCurrentLocation() {
+            var posOptions = {
+                timeout: 10000,
+                enableHighAccuracy: false
+            };
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+                    _currentLocation = new GeolocationModel({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                    $rootScope.current_location = _currentLocation;
+                    console.log('Location fetch: ');
+                    console.log(_currentLocation);
+                }, function (err) {
+                    console.log(err.message);
+                    setFallbackCoordinates(err.message);
+                });
+
+            var watchOptions = {
+                timeout: 3000,
+                enableHighAccuracy: false // may cause errors if true
+            };
+            $cordovaGeolocation.watchPosition(watchOptions)
+                .then(null,
+                function (err) {
+                    console.log(err.message);
+                    if (!_currentLocation) {
+                        setFallbackCoordinates(err.message);
+                    }
+                },
+                function (position) {
+                    _currentLocation = new GeolocationModel({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                    $rootScope.current_location = _currentLocation;
+                    console.log('Location watch: ');
+                    console.log(_currentLocation);
+                }
+                );
         }
     }
 })();
